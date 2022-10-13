32beb2a950acdac4a433b272de67f3de72140115
dancsomarci <dancsomarci@gmail.com>
2022-10-13 18:33:32 +0200

    Discussed features implemented
    Problem: data_loader not working:(

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,21 @@
 import os
 import numpy as np
+from PIL import Image
+import random
 
-for filename in os.listdir("screenshots"):
-    array = np.load(os.path.join("screenshots", filename)) # image as ndarray
-    action = filename.split("#")[0:2] # action taken by driver
+path_to_folder = "savedData"
+for filename in os.listdir(path_to_folder):
+    file = open(os.path.join(path_to_folder, filename), "rb")
+    byte_stream = file.read()
+    for byte_frame in byte_stream.split(b'\n'):
+        frame = np.frombuffer(byte_frame)
+        print(frame.shape)
+        im = Image.fromarray(frame)
+        im.save("test//{}.jpeg".format(random.random()))
+        break
+    break
+
+
 
     ## TODO ##
     # image processing (opencv python:)
--- my_control.py
+++ my_control.py
@@ -1,6 +1,5 @@
 
 import argparse
-from xmlrpc.client import Boolean
 import gym
 import numpy as np
 import pyglet
@@ -8,13 +7,6 @@ from pyglet.window import key
 from gym_duckietown.envs import DuckietownEnv
 from datetime import datetime
 import os
-
-from traitlets import Bool
-
-savepath = "foldername" #TODO set!!
-
-if not os.path.isdir(savepath):
-    os.mkdir(savepath)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env-name", default=None)
@@ -27,63 +19,110 @@ parser.add_argument("--distortion", default=False, action="store_true")
 parser.add_argument("--style", default="photos", choices=["photos", "synthetic", "synthetic-F", "smooth"])
 args = parser.parse_args()
 
-if args.env_name is None:
-    env = DuckietownEnv(
-        map_name=args.map_name,
-        draw_curve=args.draw_curve,
-        draw_bbox=args.draw_bbox,
-        domain_rand=args.domain_rand,
-        frame_skip=args.frame_skip,
-        distortion=args.distortion,
-        style=args.style,
-    )
-else:
-    env = gym.make(args.env_name)
+class Camera:
+    def __init__(self):
+        self.file = None
 
-def resetEnv():
-    env.reset()
-    env.render()
+    def is_ready(self):
+        return self.file is not None
 
-resetEnv()
+    def init(self, save_path):
+        self.file = open(os.path.join(save_path, "{}".format(datetime.now().timestamp())), "ab")
 
-@env.unwrapped.window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.BACKSPACE: # BACKSPACE==RESET
-        resetEnv()
-    
-    if symbol == key.P: # Start exporting when the P key is pressed
-        Exporter.shoud_export = not Exporter.shoud_export
+    # frame is an ndarray
+    def save_frame(self, frame):
+        if self.file is None:
+            return
+        self.file.write(frame.tobytes())
+        self.file.write("\n".encode('utf-8'))
 
-# Register a keyboard handler
-key_handler = key.KeyStateHandler()
-env.unwrapped.window.push_handlers(key_handler)
+    def save_film(self):
+        if self.is_ready():
+            self.file.close()
+            self.file = None
 
-import csv
+    def delete_film(self):
+        if self.is_ready():
+            self.file.close()
+            os.remove(self.file.name)
+            self.file = None
 
-class Exporter(object):
-    shoud_export = False
 
-def update(dt):
-    action = np.array([0.0, 0.0])
+class Controller:
+    def __init__(self):
+        if args.env_name is None:
+            self.env = DuckietownEnv(
+                map_name=args.map_name,
+                draw_curve=args.draw_curve,
+                draw_bbox=args.draw_bbox,
+                domain_rand=args.domain_rand,
+                frame_skip=args.frame_skip,
+                distortion=args.distortion,
+                style=args.style,
+            )
+        else:
+            self.env = gym.make(args.env_name)
 
-    if key_handler[key.UP] or key_handler[key.W]:
-        action += np.array([1, 0])
-    if key_handler[key.DOWN] or key_handler[key.S]:
-        action += np.array([-1, 0])
-    if key_handler[key.LEFT] or key_handler[key.A]:
-        action += np.array([-0.5, 3])
-    if key_handler[key.RIGHT] or key_handler[key.D]:
-        action += np.array([-0.5, -3])
-    if key_handler[key.SPACE]:
-        action = np.array([0, 0])
+        self.camera = Camera()
+        self.save_path = "savedData" #TODO set!!
 
-    obs, reward, done, info = env.step(action) # obs is the image seen or maybe after taking the action but it shouldn't matter because of high framerate
+        if not os.path.isdir(self.save_path):
+            os.mkdir(self.save_path)
 
-    if Exporter.shoud_export:
-        np.save(os.path.join(savepath, "{}#{}#{}".format(action[0], action[1], datetime.now().timestamp())), obs)
+        self.env.render()
 
-    env.render()
+        @self.env.unwrapped.window.event
+        def on_key_press(symbol, modifiers):
+            if symbol == key.BACKSPACE: # BACKSPACE==RESET
+                self.resetEnv()
 
-pyglet.clock.schedule_interval(update, 1.0 / env.unwrapped.frame_rate)
-pyglet.app.run()
-env.close()
+            if symbol == key.ESCAPE:
+                self.stop()
+            
+            if symbol == key.P or symbol == key.LSHIFT: # Start exporting when the P key or left-Shift is pressed
+                if not self.camera.is_ready():
+                    self.camera.init(self.save_path)
+                else:
+                    if input("Do you want to save the data? (y/n)") == "y":
+                        self.camera.save_film()
+                    else:
+                        self.camera.delete_film()
+
+        self.key_handler = key.KeyStateHandler() # these 2 lines must be after "def on_key_press" !!!
+        self.env.unwrapped.window.push_handlers(self.key_handler)
+
+    def update(self, dt):
+        action = np.array([0.0, 0.0])
+
+        if self.key_handler[key.W]:
+            action += np.array([1, 0])
+        if self.key_handler[key.S]:
+            action += np.array([-1, 0])
+        if self.key_handler[key.A]:
+            action += np.array([-0.5, 3])
+        if self.key_handler[key.D]:
+            action += np.array([-0.5, -3])
+        if self.key_handler[key.SPACE]:
+            action = np.array([0, 0])
+
+        obs, reward, done, info = self.env.step(action) # obs is the image seen or maybe after taking the action but it shouldn't matter because of high framerate
+
+        self.camera.save_frame(obs)
+        print(obs.shape)
+
+        self.env.render()
+
+    def start(self):
+        pyglet.clock.schedule_interval(self.update, 1.0 / self.env.unwrapped.frame_rate)
+        pyglet.app.run()
+
+    def stop(self):
+        self.env.close()
+
+    def resetEnv(self):
+        self.env.reset()
+        self.env.render()
+
+
+controller = Controller()
+controller.start()
